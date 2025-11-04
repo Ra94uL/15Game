8611067f385761f652ed23d10888576cc1ff2008
maruf-89 <maruf89@hotmail.com>
2025-11-04 12:28:58 +0100

    Korrekt Game15.java

--- game15/src/Game15/Game15.java
+++ game15/src/Game15/Game15.java
@@ -1,7 +1,11 @@
-package Game15;
+package game15;
 
-class Game15 {
-    static void main() {
-        IO.println("Hej från main-branchen!");
+import javax.swing.*;
+
+//Startklass för 15-spelet
+public class Game15 {
+    public static void main(String[] args) {
+        GameModel model = new GameModel(4, 4);
+        GameView view = new GameView(model);
     }
 }
